c4615b0c62fc8d421031b4934b517a636473e643
Riya1356 <108179852+Riya1356@users.noreply.github.com>
2022-11-21 05:59:24 -0800

    Delete Task4.java

--- Task4.java
+++
@@ -1,8 +0,0 @@
-public class Task4 {
-    public static void main(String[] args){
-        int a=20;
-        int b=30;
-        int add= a+b;
-        System.out.println("The Addition is :" +add);
-    }
-}
